83a9e5b12219081672ed911c1f6c54a6031960e1
Tyler Dickens <tyler.william.dickens@gmail.com>
2021-05-24 10:36:48 -0700

    Final with RGB and HEX flippers

--- color_flipper/ColorFlipper.js
+++ color_flipper/ColorFlipper.js
@@ -1,17 +1,19 @@
-const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const colors = [];
 const btn = document.getElementById("btn");
 const color = document.querySelector(".color");
 
 btn.addEventListener("click", function () {
-  const randomNumber = getRandomNumber();
-  // console.log(randomNumber);
+  
+  for(let i = 0; i<3; i++){
+    colors[i] = getRandomNumber();
+  }
 
-  document.body.style.backgroundColor = colors[randomNumber];
-  color.textContent = colors[randomNumber];
+  document.body.style.backgroundColor = "rgb("+ colors[0] + "," + colors[1] + "," + colors[2] + ")";
+  color.textContent = "RGB("+ colors[0] + "," + colors[1] + "," + colors[2] + ")";
 });
 
 function getRandomNumber() {
-  return Math.floor(Math.random() * colors.length);
+  return Math.floor(Math.random() * 255);
 }
 
 
