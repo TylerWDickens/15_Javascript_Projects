385010eba3d1202a890ee6df370574debb9cde45
Tyler Dickens <tyler.william.dickens@gmail.com>
2021-05-24 22:06:07 -0700

    Reorganized folder

---
+++ 01-color-flipper/TD Solution/ColorFlipper.js
@@ -0,0 +1,19 @@
+const colors = [];
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+btn.addEventListener("click", function () {
+  
+  for(let i = 0; i<3; i++){
+    colors[i] = getRandomNumber();
+  }
+
+  document.body.style.backgroundColor = `RGB(${colors[0]}, ${colors[1]}, ${colors[2]})`;
+  color.textContent = `RGB(${colors[0]}, ${colors[1]}, ${colors[2]})`;
+});
+
+function getRandomNumber() {
+  return Math.floor(Math.random() * 255);
+}
+
+
